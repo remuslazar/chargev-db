10159bc6c7d8d04e18c62473e4f291ccd0ca09c0
Remus Lazar <remus@lazar.info>
2018-04-14 21:52:29 +0200

    ChargeEvents view: make sorting configurable (either updatedAt or timestamp)

--- app/controllers/chargeevents.controller.ts
+++ app/controllers/chargeevents.controller.ts
@@ -7,11 +7,22 @@ router.get('/', async (req, res, next) => {
   try {
     // const conditions = {source: {$ne: 1}}; // CKCheckInSource.goingElectricSync
     // const conditions = { 'deleted': { $ne: true } };
+
+    const sort = <any>{};
+    if (req.query['sort']) {
+      const fieldName = <string>req.query['sort'];
+      sort[fieldName] = -1;
+    } else {
+      sort.timestamp = -1;
+    }
+
     const conditions = {};
     const count = await ChargeEvent.count(conditions);
     const events = await ChargeEvent
-        .find(conditions, {}, {limit: 100, sort: {'timestamp': -1}})
-        .populate('user', 'nickname recordName');
+        .find(conditions)
+        .populate('user', 'nickname recordName')
+        .limit(100)
+        .sort(sort);
     res.render('chargeevents', {
       count: count,
       events: events,
