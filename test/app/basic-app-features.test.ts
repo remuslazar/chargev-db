9a44956b0b29fe35a481047dc874ae3c3190c533
Remus Lazar <remus@lazar.info>
2018-04-21 13:54:13 +0200

    API Endpoint should return JSON data test implemented

--- test/app/basic-app-features.test.ts
+++ test/app/basic-app-features.test.ts
@@ -2,6 +2,7 @@ import {app} from "../../app/server";
 import * as chai from 'chai';
 import request = require('request-promise');
 import {StatusCodeError} from "request-promise/errors";
+import {TestAuthService} from "../auth/test-auth.service";
 
 describe('App Basic Features', function() {
 
@@ -26,6 +27,23 @@ describe('App Basic Features', function() {
   it('should respond to HTTP requests', async() => {
     const result = await request.get(baseURL);
     chai.expect(result).contain('chargev-db');
+  });
+
+  it('should always return JSON data', async() => {
+    const jwt = await (new TestAuthService()).getTestAuthJTW();
+    try {
+      const headers = {Authorization: 'Bearer ' + jwt};
+      await request.get(baseURL + 'api/v1/nonExistingEndpoint', {json: true, headers: headers});
+      chai.expect.fail('should not return 200');
+    } catch (err) {
+      if (err instanceof StatusCodeError) {
+        chai.expect(err.statusCode).eql(404);
+        const response = err.response as any;
+        chai.expect(response.body.success).false;
+        return;
+      }
+      throw err;
+    }
   });
 
   it('should respond with 403 on the API endpoint base URL', async() => {
