remuslazar/chargev-db

commit e9320d43d7626abd288e7e4eb8e91e12c19a0b63

npm upgrade, jade to pug and other major updates
various packages updated to newer versions. The codebase was also refactored
to address some issues with the TSC compiler.

app/controllers/api.controller.ts
@@ -49,11 +49,12 @@ export interface GetEventsResponse {
 }
 
 // API endpoints
-router.get('/events', async (req: AppRequest, res: Response, next: NextFunction) => {
+router.get('/events', async (req: Request, res: Response, next: NextFunction) => {
 
   const conditions: any[] = [];
+  const appReq = req as AppRequest;
 
-  setReadACLs(req.clientInfo, conditions);
+  setReadACLs(appReq.clientInfo, conditions);
 
   // changed-since option
   if (req.query['changed-since'] || req.query['change-token']) {
@@ -139,12 +140,13 @@ router.get('/events', async (req: AppRequest, res: Response, next: NextFunction)
 /**
  * fetch latest inserted (own) record
  */
-router.get('/events/latest', async (req: AppRequest, res: Response, next: NextFunction) => {
+router.get('/events/latest', async (req: Request, res: Response, next: NextFunction) => {
   const conditions: any[] = [];
 
-  setReadACLs(req.clientInfo, conditions);
+  const appReq = req as AppRequest;
+  setReadACLs(appReq.clientInfo, conditions);
 
-  conditions.push({source: req.clientInfo.source});
+  conditions.push({source: appReq.clientInfo.source});
 
   try {
     const queryConditions = { $and: conditions };
@@ -166,10 +168,11 @@ export interface DeleteEventsResponse {
   deletedRecordCount: number;
 }
 
-router.delete('/events', async (req: AppRequest, res: Response, next: NextFunction) => {
+router.delete('/events', async (req: Request, res: Response, next: NextFunction) => {
+  const appReq = req as AppRequest;
   try {
     const conditions: any[] = [];
-    setWriteACLs(req.clientInfo, conditions);
+    setWriteACLs(appReq.clientInfo, conditions);
     const response = await ChargeEvent.remove({$and: conditions}) as any;
     res.json(<DeleteEventsResponse>{
       deletedRecordCount: response.result.n,
@@ -190,14 +193,14 @@ export interface PostEventsResponse {
   deletedRecordCount: number;
 }
 
-router.post('/events', async (req: AppRequest, res: Response, next: NextFunction) => {
-
+router.post('/events', async (req: Request, res: Response, next: NextFunction) => {
+  const appReq = req as AppRequest;
   try {
     const eventsPayload = req.body as PostEventsPayload;
 
     let ChargeEventType: Model<any>;
-    switch (req.clientInfo.type) {
-      case 'CheckIn':
+    switch (appReq.clientInfo.type) {
+      case 'appReq':
         ChargeEventType = CheckIn;
         break;
       case 'Ladelog':
@@ -208,18 +211,18 @@ router.post('/events', async (req: AppRequest, res: Response, next: NextFunction
         break;
       default:
         // noinspection ExceptionCaughtLocallyJS
-        throw new Error(`ChargeEvent type: ${req.clientInfo.type} not valid`);
+        throw new Error(`ChargeEvent type: ${appReq.clientInfo.type} not valid`);
     }
 
     const writeConditions: any[] = [];
-    setWriteACLs(req.clientInfo, writeConditions);
+    setWriteACLs(appReq.clientInfo, writeConditions);
 
     let savedRecords: any[] = [];
     if (eventsPayload.recordsToSave) {
       const eventsToSave = eventsPayload.recordsToSave.map(eventData => {
 
         const newChargeEvent = new ChargeEventType(eventData);
-        newChargeEvent.source = req.clientInfo.source;
+        newChargeEvent.source = appReq.clientInfo.source;
         return newChargeEvent;
       });
       const insertedCheckIns = await ChargeEventType.insertMany(eventsToSave);

app/app.ts
@@ -37,9 +37,10 @@ app.set('views', path.join(__dirname, '../../app/views'));
 app.use(express.static(path.join(__dirname, '../../app/public')));
 
 // we want to log the clientID of the JWT auth token or auth failures
-logger.token('jwtAuthInfo', (req: AppRequest) => {
-  return req.clientInfo ? `[API clientID: ${req.clientInfo.clientID}]`
-      : req.authError ? `[API auth error: ${req.authError}]` : '';
+logger.token('jwtAuthInfo', (req: Request) => {
+  const appRequest = req as AppRequest;
+  return appRequest.clientInfo ? `[API clientID: ${appRequest.clientInfo.clientID}]`
+      : appRequest.authError ? `[API auth error: ${appRequest.authError}]` : '';
 });
 
 // don't log the static stuff
@@ -47,7 +48,7 @@ if (process.env.NODE_ENV !== 'test') {
   app.use(logger(':method :url :status :response-time ms - :res[content-length] :jwtAuthInfo'));
 }
 
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.set('trust proxy', 'loopback, linklocal, uniquelocal');
 
 app.use(bodyParser.json({limit: '50mb'}));
@@ -99,7 +100,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error.pug');
 });
 
 (mongoose as any).Promise = global.Promise;
@@ -123,7 +124,7 @@ if (!MONGODB_URI) {
   });
 }
 
-mongoose.connection.on("error", (err) => {
+mongoose.connection.on("error.pug", (err) => {
   console.log(`MongoDB connection error: ${err.message}. Please make sure MongoDB is running.`);
   process.exit(1);
 });

app/auth/jwt-auth.middleware.ts
@@ -36,27 +36,28 @@ const getTokenfromHeaderOrQuerystring = (req: Request): string|null => {
  * @param {e.NextFunction} next
  * @returns {Promise<Response>}
  */
-export const jwtAuth = async (req: AppRequest, res: Response, next: NextFunction) => {
+export const jwtAuth = async (req: Request, res: Response, next: NextFunction) => {
   const authService = new JWTAuthService(app.get(API_JWT_SECRET));
 
   // check header or url parameters or post parameters for token
   const token = getTokenfromHeaderOrQuerystring(req);
+  const appReq = req as AppRequest;
 
   // decode token
   if (token) {
     try {
-      req.clientInfo = await authService.verifyAndDecode(token);
+      appReq.clientInfo = await authService.verifyAndDecode(token);
       next();
     } catch (err) {
-      req.authError = `Failed to authenticate token: ${err.toLocaleString()}`;
-      return res.status(403).send({ success: false, message: req.authError });
+      appReq.authError = `Failed to authenticate token: ${err.toLocaleString()}`;
+      return res.status(403).send({ success: false, message: appReq.authError });
     }
   } else {
     // if there is no token, return an error
-    req.authError = 'No auth token provided';
+    appReq.authError = 'No auth token provided';
     return res.status(403).send({
       success: false,
-      message: req.authError,
+      message: appReq.authError,
     });
   }
 

app/controllers/root.controller.ts
@@ -3,7 +3,7 @@ import {Router, Request, Response} from 'express';
 const router: Router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.render('index', {
+  res.render('index.pug', {
   });
 });
 

commit eb7ad7df812e95fda57309377fff2d8f4e24656c

Regression introduced by e9320d4 fixed

app/controllers/api.controller.ts
@@ -200,7 +200,7 @@ router.post('/events', async (req: Request, res: Response, next: NextFunction) =
 
     let ChargeEventType: Model<any>;
     switch (appReq.clientInfo.type) {
-      case 'appReq':
+      case 'CheckIn':
         ChargeEventType = CheckIn;
         break;
       case 'Ladelog':